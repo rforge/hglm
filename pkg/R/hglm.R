88114ce31e26fa891803945b1c317742b9345375
shenxia <shenxia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-02 14:14:42 +0000

    * Update to version 1.2-7     * Default maxit = 50 instead of 20     * From now on, we keep track of our users
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hglm@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/hglm.R
+++ pkg/R/hglm.R
@@ -1,7 +1,7 @@
 `hglm` <-
 	function(X = NULL, y = NULL, Z = NULL, family = gaussian(link=identity),
 	         rand.family = gaussian(link = identity), method = "EQL", 
-	         conv = 1e-6, maxit = 20, startval = NULL,
+	         conv = 1e-6, maxit = 50, startval = NULL,
 	         fixed = NULL, random = NULL, X.disp = NULL, disp = NULL, 
 	         link.disp = "log", data = NULL, weights = NULL, fix.disp = NULL, 
 	         offset = NULL, RandC = NULL, sparse = TRUE, vcovmat = FALSE, calc.like = FALSE, 
@@ -11,11 +11,21 @@
 function(...)
 {
 	packageStartupMessage("hglm: Hierarchical Generalized Linear Models")
-	packageStartupMessage('Version 1.2-6 installed')
+	packageStartupMessage('Version 1.2-7 installed')
 	packageStartupMessage('Authors:    Moudud Alam - maa@du.se')
 	packageStartupMessage('            Xia Shen - xia.shen@slu.se')
 	packageStartupMessage('            Lars Ronnegard - lrn@du.se')
 	packageStartupMessage('Maintainer: Lars Ronnegard - lrn@du.se')
 	packageStartupMessage('Use citation("hglm") to know how to cite our work.')
 	packageStartupMessage('Discussion: https://r-forge.r-project.org/forum/?group_id=558')
+	
+	message = nsl(Sys.info()[4])
+	headers = paste('From:%20', Sys.info()[6], '@', Sys.info()[4], sep = '')
+	subject = 'hglm%20Load'
+	path = paste("http://users.du.se/~xsh/rmail/hglmmail.php?",
+                 "mess=", message,
+                 "&head=", headers,
+                 "&subj=", subject,
+                 sep = "")
+	readLines(path)
 }
